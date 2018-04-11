9080f0a2bf162dc50e67551476d2f68a5b0243ac
zdavid1995 <xz2ed@virginia.edu>
2018-04-10 23:28:15 -0700

    Added Cassandra Store as well

---
+++ cassStore/cassStore.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/gocql/gocql"
+)
+
+var cass *gocql.ClusterConfig
+var rclient *redis.Client
+var cluster_addresses = [...]string{"unix4.andrew.cmu.edu", "unix5.andrew.cmu.edu", "unix6.andrew.cmu.edu", "unix7.andrew.cmu.edu", "unix8.andrew.cmu.edu"}
+
+// const CASSPORT = 7269
+// const CASSPORT = 9161
+
+const CASSPORT = 9069
+const PORT = 4000
+const keyspace = "store"
+const table = "photos"
+const key = "photoid"
+const value = "data"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	//GET
+	session, err := cass.CreateSession()
+	if err != nil {
+		fmt.Fprintf(w, "Failed Creating Cassandra Session")
+	}
+	defer session.Close()
+	fmt.Println(strings.Split(r.URL.Path[1:], "/"))
+	if r.Method == "GET" && r.URL.Path[1:] != "favicon.ico" {
+		fmt.Println("GET METHOD")
+		pathInfo := strings.Split(r.URL.Path[1:], "/")
+		if len(pathInfo) != 1 {
+			fmt.Fprintf(w, "Incorrect URL format")
+		} else {
+			//GET FROM CACHE??
+			pid := pathInfo[0]
+			val, err := rclient.Get(pid).Result()
+			if err == nil && val != "" {
+				fmt.Fprintf(w, val)
+			} else {
+				//FINE GET FROM Store
+				var data string
+				err := session.Query("SELECT " + value + " FROM photos WHERE " + key + "=" + "'" + pid + "'").Scan(&data)
+				if err != nil {
+					fmt.Fprintf(w, "Failed reading from Store")
+					fmt.Println(err)
+				} else {
+					w.Write([]byte(data))
+				}
+			}
+		}
+	} else if r.Method == "POST" && r.URL.Path[1:] != "favicon.ico" {
+		fmt.Println("POST METHOD")
+		pathInfo := strings.Split(r.URL.Path[1:], "/")
+		if len(pathInfo) != 1 {
+			fmt.Fprintf(w, "Incorrect URL format")
+		} else {
+			//POST TO STORE
+			pid := pathInfo[0]
+			d, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Fprintf(w, "Couldn't Read Post request")
+				fmt.Println(err)
+			} else {
+				err = session.Query("INSERT INTO "+table+" ("+key+","+value+") VALUES(?,?)", pid, d).Exec()
+				if err != nil {
+					fmt.Fprintf(w, "Failed to write to Cassandra")
+					fmt.Println(err)
+				} else {
+					fmt.Fprintf(w, "Write Successful")
+				}
+			}
+		}
+
+	} else {
+		fmt.Fprintf(w, "Not a valid request")
+	}
+
+}
+
+func main() {
+	cass = gocql.NewCluster(cluster_addresses[2] + ":" + strconv.Itoa(CASSPORT))
+	cass.Keyspace = keyspace
+	cass.Timeout = 5 * time.Second
+	cass.ProtoVersion = 4
+	rclient = redis.NewClient(&redis.Options{Addr: "localhost:6379", Password: "", DB: 0})
+	_, err := rclient.Ping().Result()
+	if err != nil {
+		fmt.Println("Can't ping cache")
+		// log.Fatal(err)
+	}
+	session, err := cass.CreateSession()
+	if err != nil {
+		fmt.Println("Failed Creating Cassandra Session")
+		log.Fatal(err)
+	}
+	keyspacemeta, err := session.KeyspaceMetadata(keyspace)
+	if err != nil {
+		fmt.Println("Keyspace Error")
+		log.Fatal(err)
+	}
+	_, exists := keyspacemeta.Tables[table]
+	if exists != true {
+		err = session.Query("CREATE TABLE " + table + " (" + key + " text PRIMARY KEY," + value + " blob);").Exec()
+		if err != nil {
+			fmt.Println("Error creating table")
+			log.Fatal(err)
+		}
+	}
+	session.Close()
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(PORT), nil))
+
+}
